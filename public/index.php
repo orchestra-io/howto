8af5e3bb1acd4fe39e4e98c961309593a35621e9
Eamon Leonard <eamonleonard@87-198-25-223.ptr.magnet.ie>
2011-03-13 19:40:53 +0000

    added some branding and links to documentation etc

--- public/index.php
+++ public/index.php
@@ -1,23 +1,44 @@
-<h2>We are now in the presence of the HowTo Orchestra Application!</h2>
-<p>
-  If you are seeing this, congratulations, you now have your first application running.
-</p>
+<html>
+	<head>
+		<link rel="stylesheet" type="text/css" href="styles.css" media="screen"/> 
+	</head>
+	<body>
+		<div id="container">
+			<div id="logo">
+				<img src="images/orchestra_logo_on_light.png">
+			</div>
+			<div id="content">
+				<h2>Seriously, how easy was that?</h2>
+				<p>This is just a simple bit of code, deployed from a <a href="https://github.com/orchestra-io/howto">repo on GitHub</a> to show you how  you can be quickly up and running.</p>
+				<p>
+					You can delete this deployed app from your <a href="https://my.orchestra.io/apps">app dashboard</a> by clicking "Terminate".
+				</p>
+				<p>Now, go! <a href="https://my.orchestra.io/apps/create/free?">Start deploying</a> from your own Git or Subversion repositories. We've got some easy to read documentation on that, if you need it:
+				<ul>
+					<li><a href="https://orchestra.tenderapp.com/kb/getting-started/deploy-with-git">Deploy onto Orchestra from Git</a></li>
+					<li><a href="https://orchestra.tenderapp.com/kb/getting-started/deploy-with-subversion">Deploy onto Orchestra from Subversion</a></li> 
+				</ul>
+				<h2>Test data from PHP</h2>
+				<p>
+				<?php
+					echo "<p>Your app is run on PHP version: " . PHP_RELEASE_VERSION . "<br/></p>";
+					echo "<p>The IP of your app is: " . $_SERVER['REMOTE_ADDR'] . "<br/></p>";
+					echo "<p>Temp dir available to your app is: " . sys_get_temp_dir() . "</p>";
+				?>
+
+				</p>
+		
+				<h2>Code from above</h2>
 <?php
-    $version = PHP_RELEASE_VERSION;
-    $ip      = $_SERVER['REMOTE_ADDR'];
-    $temp    = sys_get_temp_dir();
-?>
-
-<h2>Test data from PHP</h2>
-<p>You are running PHP version <?php echo $version; ?> and you appear to have the ip <?php echo htmlspecialchars($ip); ?>. The temp dir is <?php echo $temp; ?></p>
-
-<h2>Code</h2>
-<p>The code that ran above is:
- <pre>
-  <code>
-  $version = PHP_RELEASE_VERSION;
-  $ip      = $_SERVER['REMOTE_ADDR'];
-  $temp    = sys_get_temp_dir();
-  </code>
- </pre>
-</p>
+$code = highlight_string('
+<?php 
+	$version = PHP_RELEASE_VERSION;
+	$ip = $_SERVER["REMOTE_ADDR"];
+	$temp = sys_get_temp_dir();
+?>',1);
+echo $code;
+?>				
+			<div>
+		</div>
+	</body>
+</html>
