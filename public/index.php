7ea46f459acd3070590e0ee800f004ed9c0d9515
gwoo <gwoo@orchestra.io>
2012-01-12 16:47:16 -0800

    david making stuff up again.

--- public/index.php
+++ public/index.php
@@ -24,20 +24,18 @@
 				<?php
                     $version = phpversion();
 					echo "<p>Your app is running on PHP version: " . $version . "<br/></p>";
-					echo "<p>The app IP is: " . $_SERVER['HTTP_X_FORWARDED_SERVER'] . "<br/></p>";
-					echo "<p>The client IP is : " . $_SERVER['HTTP_X_FORWARDED_HOST'] . "<br/></p>";
+					echo "<p>The app IP is: " . $_SERVER['SERVER_ADDR'] . "<br/></p>";
+					echo "<p>The client IP is : " . $_SERVER['HTTP_X_FORWARDED_FOR'] . "<br/></p>";
 					echo "<p>Temp dir available to your app is: " . sys_get_temp_dir() . "</p>";
 				?>
-
 				</p>
-
 				<h2>Code from above</h2>
 <?php
 $code = highlight_string('
 <?php
 	$version = phpversion();
-	$ip = $_SERVER["HTTP_X_FORWARDED_SERVER"];
-	$ip = $_SERVER["HTTP_X_FORWARDED_HOST"];
+	$ip = $_SERVER["SERVER_ADDR"];
+	$ip = $_SERVER["HTTP_X_FORWARDED_FOR"];
 	$temp = sys_get_temp_dir();
 ?>',1);
 echo $code;
